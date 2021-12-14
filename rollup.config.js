65d1f221b9472b765980d618c54476511c7ea04a
shoppingzh <1049262057@qq.com>
2021-12-14 18:33:25 +0800

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,13 @@
+import babel from '@rollup/plugin-babel'
+import banner from 'rollup-plugin-banner'
+import { uglify } from 'rollup-plugin-uglify'
+
+const bannerStr = `
+cool-parser
+A question parser
+by xpzheng
+`
+
 export default {
   input: 'src/index.js',
   output: {
@@ -7,5 +17,15 @@ export default {
   },
   watch: {
     buildDelay: 500
-  }
+  },
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env']
+    }),
+    banner(bannerStr),
+    uglify({
+      sourcemap: false
+    })
+  ]
 }
